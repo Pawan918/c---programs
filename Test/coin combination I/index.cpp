595dc0408daf63d9fac85e55bbe372e37cdc9b16
Pawan918 <74725223+Pawan918@users.noreply.github.com>
2022-01-24 21:55:26 +0530

    done

---
+++ Test/coin combination I/index.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    int n,x;
+    cin>>n>>x;
+    vector<int> coins(n);
+    vector<ll> times(n+1,0);
+    for(int i=0;i<n;i++){
+        cin>>coins[i];
+    }
+    
+    return 0;
+}
